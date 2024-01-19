3ac967e9ac7c20159f645a5365b820c70a742c0f
sahara.ap <sahara.ap13@gmail.com>
2024-01-19 11:24:48 +0300

    Добавлен способ получения селекторов из стора через createSelector()

--- src/store/offer-data/offer-data-selectors.ts
+++ src/store/offer-data/offer-data-selectors.ts
@@ -1,13 +1,24 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { LoadingDataStatus, NameSpace } from '../../const';
 import { Favorite, Offer, SelectedOffer } from '../../types/offer';
 import ReviewType from '../../types/review';
 import { State } from '../../types/state';
+import { OffersData } from '../../types/sliceTypes';
 
-const getOffers = (state: Pick<State, NameSpace.Data>): Offer[] => state[NameSpace.Data].offers;
+// const getOffers = (state: Pick<State, NameSpace.Data>): Offer[] => state[NameSpace.Data].offers;
+// использую новый способ для создания селектора. Он позволяет дополнительно обрабатывать данные, которые получены из стейта стора
+const getOffersSelector = (state: Pick<State, NameSpace.Data>): OffersData => state[NameSpace.Data];
+const getOffers = createSelector([getOffersSelector], (stateData: OffersData) => stateData.offers);
+
 const getSelectedOffer = (state: Pick<State, NameSpace.Data>): SelectedOffer | null => state[NameSpace.Data].selectedOffer;
 const getNearPlaces = (state: Pick<State, NameSpace.Data>): Offer[] => state[NameSpace.Data].nearPlaces;
-const getReviews = (state: Pick<State, NameSpace.Data>): ReviewType[] => state[NameSpace.Data].reviews;
 const getFavs = (state: Pick<State, NameSpace.Data>): Favorite[] => state[NameSpace.Data].favs;
+
+//получаю отсортированные по дате список Review
+const getReviews = (state: Pick<State, NameSpace.Data>): ReviewType[] => state[NameSpace.Data].reviews;
+const getSortedReviews = createSelector([getReviews], (reviews:ReviewType[]) => reviews.slice()
+  .sort((reviewA, reviewB) => Date.parse(reviewB.date) - Date.parse(reviewA.date)));
+
 
 const getReviewStatusSending = (state: Pick<State, NameSpace.Data>): LoadingDataStatus => state[NameSpace.Data].reviewStatusSending;
 const getIsOffersLoading = (state: Pick<State, NameSpace.Data>): boolean => state[NameSpace.Data].isOffersLoading;
@@ -22,6 +33,7 @@ export {
   getNearPlaces,
   getReviews,
   getFavs,
+  getSortedReviews,
 
 
   getReviewStatusSending,
